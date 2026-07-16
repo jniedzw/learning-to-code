da5636ccf14fb93127b9b29c51eb3968e8b1aeb1
jniedzw <jamesnied.dev@gmail.com>
2026-07-16 17:27:48 +0100

    changed code to split user's first and last name

--- Week1/hello.name.py
+++ Week1/hello.name.py
@@ -1,14 +1,8 @@
 # Ask for the users name
-name = input("What is your name? ")
+name = input("What is your name? ").strip().title()
 
-#Remove any leading or trailing whitespace from str
-name = name.strip()
-
-#Capitalise users name
-name = name.title()
+#split user's name into first and last name
+first, last = name.split(" ")
 
 # Greet the user by name
-print(f"hello, {name}")
-
-
-
+print(f"Hello, {first}")
