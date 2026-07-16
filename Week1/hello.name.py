bf7bcf17620e5ab340f10a730d94751e5ee66beb
jniedzw <jamesnied.dev@gmail.com>
2026-07-16 17:10:54 +0100

    Add prompt for users name

---
+++ Week1/hello.name.py
@@ -0,0 +1,14 @@
+# Ask for the users name
+name = input("What is your name? ")
+
+#Remove any leading or trailing whitespace from str
+name = name.strip()
+
+#Capitalise users name
+name = name.title()
+
+# Greet the user by name
+print(f"hello, {name}")
+
+
+
